9a986a2db0781b8425cb6f646f73a7e96a5417bc
Ivan Atuam <i8591d@gmail.com>
2017-11-23 09:11:11 +0000

    new

--- www/js/mauta.js
+++ www/js/mauta.js
@@ -1,6 +1,85 @@
 $(document).ready(function () {
     //Section Self
 
+    var masterGroup = {
+        num: [],
+        score: [],
+        min: 1,
+        max: 59,
+        numLen:6,
+        initialize: function () {
+            for (var x = 1; x <= this.max; x++) {
+                masterGroup.num.push(x);
+                masterGroup.score.push(0);
+            }
+        }
+    }
+    let master = [];
+    let numList = [];
+
+    let tStatus = setInterval(function () {
+        for (var x = 0; x < masterGroup.numLen; x++) {
+            //let number = Math.floor(Math.random() * (masterGroup.max - masterGroup.min) + masterGroup.min);
+            numList.push(Math.floor(Math.random() * (masterGroup.max - masterGroup.min) + masterGroup.min));
+        }
+        master.push(numList);
+        console.log(numList);
+        numList = [];
+        
+    },100);
+
+   $("#stop").click(function () {
+       try {
+           clearInterval(tStatus);
+           let line = 0;
+           masterGroup.initialize();
+           $.each(master, function () {
+               let currentLine = $(this);
+
+               $.each(currentLine, function () {
+                   let currNum = 0;
+                   currNum = parseInt($(this)[0]);
+                   let curr = masterGroup.num.indexOf(currNum);
+                   let numValue = masterGroup.score[masterGroup.num.indexOf(currNum)];
+                   numValue++;
+                   //Set score
+                   masterGroup.score[masterGroup.num.indexOf(currNum)] = numValue;
+               });
+
+               line++;
+           });
+           let scr = [];
+           for (var x = 0; x <= masterGroup.numLen; x++) {
+
+               let lastnum = 0;
+               $.each(masterGroup.score, function () {
+                   let cur = parseInt($(this)[0]);
+                   if (cur > lastnum) {
+                       lastnum = cur;
+                   }
+               });
+               let item = masterGroup.score.indexOf(lastnum);
+               scr.push(masterGroup.score.indexOf(lastnum));
+               masterGroup.score.splice(masterGroup.score.indexOf(lastnum), 1);
+               //masterGroup.score = masterGroup.score.filter(item => item !== value)
+           }
+
+           let yourNumber = [];
+           $.each(scr, function () {
+               let cur = parseInt($(this)[0]);
+               yourNumber.push(masterGroup.num.indexOf(cur));
+           });
+           $.each(yourNumber, function () {
+               let content = $(this);
+               console.log(parseInt($(this)[0]));
+           });
+       } catch(ex){
+           alert(ex);
+       }
+   });
+    //  points.sort(function(a, b){return a - b});
+    //  document.getElementById("demo").innerHTML = points;
+
     window.resizeTo("500", "600");
     var dyna_mo = {};
     dyna_mo.key = 'Data';
